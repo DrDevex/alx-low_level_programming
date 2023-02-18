788e3a51fb0bd5f91ada39e4b18d720a1c6ad678
Oladapo Ahmed <oladapoahmed5@gmail.com>
2023-02-18 07:48:53 +0100

    cprogram

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
-* main - assigns a random number to variable in each time
-* it executes an prints it
-* Return: Always 0 (success)
+ *main - assigns a random number to variable in each time
+ *it executes an prints it
+ *Return: Always 0 (success)
 */
 int main(void)
 {	
