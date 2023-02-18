baf8bdda213b2c7911a675f699943f7459338ef5
Oladapo Ahmed <oladapoahmed5@gmail.com>
2023-02-18 07:54:18 +0100

    cprogram3

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
  *main - assigns a random number to variable in each time
@@ -8,7 +7,7 @@
  *Return: Always 0 (success)
  */
 int main(void)
-{	
+{
 	int n;
 
 	srand(time(0));
