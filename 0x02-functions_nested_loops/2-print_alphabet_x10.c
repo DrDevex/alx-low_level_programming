2ab03024c508bbbd9cf9f4b7932ca4148c3de7be
Oladapo Ahmed <oladapoahmed5@gmail.com>
2023-02-22 23:26:27 +0100

    fixed lowercase alphabet bug

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10(void)- prints alphabets in lowercase ten times
+ * Return: if 1 then it returns successfully or -1 for errors
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha = 'a';
+
+	while (a
+}
