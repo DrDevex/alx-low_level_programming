850e78174117465d0b9cf803a3d98ae6b574a470
Oladapo Ahmed <oladapoahmed5@gmail.com>
2023-02-22 22:45:27 +0100

    Alphabet task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main- Prints all the alphabet in lowercase
+ * Return: 1 means it runs success and returns -1 if fail
+ */
+
+int main(void)
+{
+	char i;
+
+	while (i = a; i <= z)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
