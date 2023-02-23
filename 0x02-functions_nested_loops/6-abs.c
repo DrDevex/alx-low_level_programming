6ff42f1168e55e68fcd06eb158cc0de1c6085249
Oladapo Ahmed <oladapoahmed5@gmail.com>
2023-02-23 01:33:28 +0100

    Latest push

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs- prints the absolutes value of an int
+ * @n:characters being checked
+ * Return: abs of num
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-1 * n);
+}
