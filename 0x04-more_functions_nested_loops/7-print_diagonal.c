ba10c8bec7a86d7623d23a586541641304403770
Oladapo Ahmed <oladapoahmed5@gmail.com>
2023-02-25 02:06:00 +0100

    Fixing error bugs

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,34 +1,38 @@
 #include "main.h"
 
 /**
- * print_diagonal- draws a diagonal line
- * @n: times the character should be printed
- * Return: Always 0
+ * print_diagonal- print a backlash
+ * @n: says how many backlash this function is going to print
+ * Return: The respective quantity of backlash
  */
 void print_diagonal(int n)
 {
-	if (n <= 0)
+	int i, j;
+
+	if (!(n <= 0))
 	{
-		_putchar(\'n');
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				if (j <= i)
+				{
+					if (j == i)
+						_putchar('\\');
+					else
+						_putchar(' ');
+				}
+				else
+				{
+					if (j == n)
+						_putchar('\n');
+				}
+			}
+		}
+		_putchar('\n');
 	}
 	else
 	{
-		int i, j;
-
-		for (i = 0; i < n; i++)
-		{
-			for (j = o; j < n; j++)
-			{
-				if (j == i)
-				{
-					_putchar('\\');
-				}
-				else if (j < i)
-				{
-					_putchar(' ');
-				}
-			}
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
 }
